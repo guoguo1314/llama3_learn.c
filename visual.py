0014ffb7af3de84a654fc425bb4195912e39b3d5
guoguo1314 <57977189+guoguo1314@users.noreply.github.com>
2024-04-21 11:41:50 +0800

    Update visual.py

--- visual.py
+++ visual.py
@@ -1,7 +1,8 @@
 import torch
 
-# 加载模型权重文件
-model = torch.load('/mnt/workspace/llama3.c/download/Meta-Llama-3-8B/original/consolidated.00.pth')
+# 加载权重文件为字典形式
+state_dict = torch.load('Meta-Llama-3-8B/original/consolidated.00.pth', map_location='cpu')
 
-# 查看模型结构和包含的层
-print(model)
+# 打印字典键（通常这些键对应模型各层的名称）
+for key in state_dict.keys():
+    print(key)
