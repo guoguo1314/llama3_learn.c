c7f997072ce8a2391242668aeedeeff63ad7c93e
guoguo1314 <57977189+guoguo1314@users.noreply.github.com>
2024-04-21 15:47:10 +0800

    Update run.c

--- run.c
+++ run.c
@@ -907,11 +907,11 @@ int main(int argc, char *argv[]) {
 
     // default parameters
     char *checkpoint_path = NULL;  // e.g. out/model.bin
-    char *tokenizer_path = "/mnt/workspace/llama3.c/llama3-tokenizer.bin";
+    char *tokenizer_path = "llama3-tokenizer.bin";
     float temperature = 1.0f;   // 0.0 = greedy deterministic. 1.0 = original. don't set higher
     float topp = 0.9f;          // top-p in nucleus sampling. 1.0 = off. 0.9 works well, but slower
     int steps = 256;            // number of steps to run for
-    char *prompt = "I am LuoYing,";        // prompt string
+    char *prompt = "I am xxxx,";        // prompt string
     unsigned long long rng_seed = 0; // seed rng with time by default
     char *mode = "generate";    // generate|chat
     char *system_prompt = NULL; // the (optional) system prompt to use in chat mode
